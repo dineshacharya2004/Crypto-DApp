2f0a3339a744fca366716785a0ce474cc00c5503
dineshacharya2004@gmail.com <dineshacharya2004@gmail.com>
2024-04-28 18:39:58 +0530

    Components initialisation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
+import {Navbar, Welcome, Footer, Services, Transactions} from './components';
 
-
-const App = () => {
- 
-
-  return (
-    
-      <div>
-       <h1 className="text-3xl font-bold underline">
-         Hello world!
-       </h1>
-       </div>
-    
-  )
-}
+const App = () => (
+  <div className="min-h-screen">  
+        {/*a CSS class that ensures it covers at least the entire height of the screen when rendered in the browser*/}
+    <div className="gradient-bg-welcome">
+        {/*class name "gradient-bg-welcome" suggests that it might be used to apply a gradient background to an element, perhaps for a welcome section or some other prominent area of your user interface*/}
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App
