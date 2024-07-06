d3cde607f83cc0c4bf00f0eeb4835810f42e42be
dineshacharya2004 <dineshacharya2004@gmail.com>
2024-07-07 01:22:29 +0530

    Navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+// tailwind.config.js
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -6,4 +7,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
